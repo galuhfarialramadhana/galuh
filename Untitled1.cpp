d8a287012b680f6cba5fce6530c9b751fd200f3e
galuhfarialramadhana <Galuhramadhana15@gmail.com>
2018-01-20 14:57:05 +0700

    class

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class laptop {
+    public:
+    	void jenislptp(string jenis);
+        string merek;
+        void ukuranlptp(string ukuran);
+    
+      
+        };
+        
+int main(){
+}
