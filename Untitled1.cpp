a4f5e55a49c124ae201f67dffb0937214d73df9b
galuhfarialramadhana <Galuhramadhana15@gmail.com>
2018-01-20 14:58:10 +0700

    isi

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -12,4 +12,32 @@ class laptop {
         };
         
 int main(){
+    laptop notebook, standard, netbook, touchscreen; 
+
+	notebook.merek			="Acer ";
+	standard.merek			="Lenovo ";
+	netbook.merek			="Asus ";
+	touchscreen.merek		="Toshiba ";
+
+	
+	cout << "notebook: \n\n";
+	cout << (notebook.merek);
+	notebook.ukuranlptp("12 inch ");
+	cout <<"===================================\n\n";
+	
+	cout << "standard: \n\n";
+	cout << (standard.merek);
+	standard.ukuranlptp("14inch ");
+	cout <<"===================================\n\n";
+	
+	cout << "netbook: \n\n";
+	cout << (netbook.merek);
+	netbook.ukuranlptp("12inch ");
+	cout <<"===================================\n\n";
+	
+	cout << "touchscreen: \n\n";
+	cout << (touchscreen.merek);
+	touchscreen.ukuranlptp("16inch ");
+	cout <<"===================================\n\n";
+	
 }
